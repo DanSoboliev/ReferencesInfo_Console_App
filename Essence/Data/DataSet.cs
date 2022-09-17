cf26bb6151b7d46351a3c848f030c7c4d53a74f2
Dan <sobolevdanulo@gmail.com>
2022-09-17 19:53:51 +0300

    Добавьте файлы проекта.

---
+++ Essence/Data/DataSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ReferencesInfo.Entities;
+
+namespace ReferencesInfo.Data {
+    [Serializable]
+    public class DataSet {
+        public List<Book> Books { get; private set; }
+        public List<Link> Links { get; private set; }
+        public DataSet() {
+            Books = new List<Book>();
+            Links = new List<Link>();
+        }
+    }
+}
